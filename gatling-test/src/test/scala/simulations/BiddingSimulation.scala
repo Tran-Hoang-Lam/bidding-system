2d2dd286f5d0c330d526b46abb15d2cb616fd8a2
lhtran <lam.hoang.tran@mgm-tp.com>
2019-11-03 01:34:28 +0700

    add gatling module and simulation test

---
+++ gatling-test/src/test/scala/simulations/BiddingSimulation.scala
@@ -0,0 +1,35 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.controller.inject.RampInjection
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef.http
+import simulations.components.Bidding
+
+import scala.concurrent.duration._
+
+class BiddingSimulation extends Simulation {
+  val biddingItem1: ScenarioBuilder = scenario("Bidding Item1")
+    .group("Bidding Item1 with new price") {
+      exec(Bidding.bid("item1"))
+    }
+
+  val biddingItem2: ScenarioBuilder = scenario("Bidding Item2")
+    .group("Bidding Item2 with new price") {
+      exec(Bidding.bid("item2"))
+    }
+
+  val steps: RampInjection = rampUsers(500) over (1 minutes)
+
+  private def env = http
+    .baseURL("http://localhost:8080/api/v1")
+    .header("Accept", "application/json")
+    .header("Content-Type", "application/json")
+    .silentResources.inferHtmlResources()
+    .maxConnectionsPerHostLikeChrome
+
+  setUp(
+    biddingItem1.inject(steps).protocols(env),
+    biddingItem2.inject(steps).protocols(env)
+  )
+}
---
+++ gatling-test/src/test/scala/simulations/components/Bidding.scala
@@ -0,0 +1,23 @@
+package simulations.components
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
+object Bidding {
+  var initialBidPrice = 90000
+  private def increasePrice(): Int = {
+    initialBidPrice += 10000
+    initialBidPrice
+  }
+
+  private def getBidPrice(id: String) = Map("bidPrice" -> increasePrice(), "id" -> id)
+
+  private def feeder(id:String): Iterator[Map[String, Any]] = Iterator.continually(getBidPrice(id))
+
+  def bid(id: String): ChainBuilder = {
+    feed(feeder(id))
+      .exec(http("Bid item").post("/bidding-item/bid?id=${id}&price=${bidPrice}"))
+      .pause(1)
+  }
+}
